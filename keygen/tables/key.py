f855c3e4b447d2474c8e59d4a24d22429db172ec
yoshapihoff <mario.nomer.1@gmail.com>
2019-01-30 10:20:09 +0500

    missed Key class and favicon

---
+++ keygen/tables/key.py
@@ -0,0 +1,15 @@
+from ..model import db
+
+
+class Key(db.Model):
+    __tablename__ = 'keys'
+
+    value = db.Column(db.String(4), primary_key=True)
+    used = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return f"<Key(value='{self.value}', used='{self.used}')>"
+
+    def __init__(self, value):
+        self.value = value
+        self.used = False
